30aa29b6382b4b1a2ffd88b24d15b1d5799753bb
Jess Smith <shisujieit@gmail.com>
2016-11-27 10:43:17 +0800

    key添加测试；

---
+++ DeveloperKeyInfo/Key.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeveloperKeyInfo
+{
+
+    class Key
+    {
+        public static string test = Local.test;
+    }
+}
